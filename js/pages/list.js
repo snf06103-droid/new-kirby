540d4a706e92bea093e299edb6ad89ddd07ffc07
윤서 <snf06103@naver.com>
2026-03-10 22:50:45 +0900

    oo

--- js/pages/list.js
+++ js/pages/list.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const selectLabel = document.querySelector('.select-label');
     const selectBox = document.querySelector('.select-box');
 
-    // 요소가 존재하는지 먼저 체크하는 습관이 중요해요!
     if (selectLabel && selectBox) {
         selectLabel.addEventListener('click', () => {
             selectBox.classList.toggle('active');
