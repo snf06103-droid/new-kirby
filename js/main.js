0bce82ac2336859abcc14d3575d0adf597f382a7
장윤서 <snf06103@naver.com>
2026-03-13 17:57:40 +0900

    룩북 슬라이드 기능 넣기

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@ document.addEventListener("DOMContentLoaded",()=>{
       //   prevEl: '.swiper-button-prev',
       // },
       pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },  
-    });
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },  
+      });
 
 
 
@@ -31,14 +31,14 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 
         var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 5,
-      spaceBetween: 30,
-      freeMode: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+          slidesPerView: 5,
+          spaceBetween: 30,
+          freeMode: true,
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+        });
 })
 
 
