d44fe9ed9fd95760765c8b750751e85c0d213e03
장윤서 <snf06103@naver.com>
2026-03-20 18:08:28 +0900

    마이페이지 자바스크립트 기능 넣는 중

--- js/pages/mypage.js
+++ js/pages/mypage.js
@@ -9,8 +9,18 @@ document.addEventListener('DOMContentLoaded', function() {
             
             // 부모에 active 클래스를 토글합니다.
             parentNotice.classList.toggle('down');
-            
-            
         });
     });
+
+    const statusButton = document.querySelector('.status-button')
+    const orderList = document.querySelector('.order-list')
+    const view = document.querySelector('.view')
+    const statusActive = document.querySelector('.status-active')
+    const customerService = document.querySelector('.customer-service')
+    const statusbar = document.querySelector('.status-bar .bar')
+    
+
+
+
+
 });
