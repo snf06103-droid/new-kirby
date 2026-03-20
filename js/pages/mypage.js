b746b2e5a7ef046501cb4a8461e87fed85d2465c
장윤서 <snf06103@naver.com>
2026-03-20 13:14:49 +0900

    마이페이지 다 만듦

---
+++ js/pages/mypage.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // 모든 '.title-box' 요소들을 가져옵니다.
+    const titleBoxes = document.querySelectorAll('.text-box');
+
+    titleBoxes.forEach(function(titleBox) {
+        titleBox.addEventListener('click', function() {
+            // 클릭한 title-box의 부모 .notice-text를 찾습니다.
+            const parentNotice = this.closest('.service-box');
+            
+            // 부모에 active 클래스를 토글합니다.
+            parentNotice.classList.toggle('down');
+            
+            
+        });
+    });
+});
